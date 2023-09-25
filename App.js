0de854c1493cbf223eee9a11ed1410ef0163e2ad
Joabe Uriel Dias Milhomens <60987355+joabediias@users.noreply.github.com>
2023-09-25 00:02:59 -0300

    V.1.0.3: Include Tabs

--- App.js
+++ App.js
@@ -3,17 +3,29 @@ import {NavigationContainer} from '@react-navigation/native';
 
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {createDrawerNavigator} from '@react-navigation/drawer';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
 
 import Contacts from './src/pages/Contacts';
 import Information from './src/pages/Information';
+import AppContacts from './src/pages/AppContacts';
 
 const Stack = createNativeStackNavigator();
 const Drawer = createDrawerNavigator();
+const Tab = createBottomTabNavigator();
+
+function Tabs() {
+  return(
+    <Tab.Navigator>
+      <Tab.Screen name = "AppContacts" component = {AppContacts}/>
+      <Tab.Screen name = "Contacts" component = {Contacts}/>
+    </Tab.Navigator>
+  );
+}
 
 export default function App() {
   return (
-     //myStack() // função utilizando o navigator Stack 
-     myDrawer() // função utilizando o navigator Drawer
+     myStack() // função utilizando o navigator Stack 
+     //myDrawer() // função utilizando o navigator Drawer
   );
 }
 
@@ -22,8 +34,8 @@ function myStack() {
       <NavigationContainer>
         <Stack.Navigator>
           <Stack.Screen 
-            name = "Contacts" 
-            component = {Contacts} />
+            name = "AppContacts" 
+            component = {Tabs} />
           <Stack.Screen 
             name = "Information" 
             component = {Information}/>
@@ -37,8 +49,8 @@ function myDrawer() {
     <NavigationContainer>
       <Drawer.Navigator>
         <Drawer.Screen 
-          name = "Contacts" 
-          component = {Contacts} />
+          name = "AppContacts" 
+          component = {Tabs} />
         <Drawer.Screen 
           name = "Information" 
           component = {Information}/>
